dd9314a2fc79fe1b6dcd324ce0cf2b892130cd8a
KaiserElite <erik.le.279@gmail.com>
2025-03-27 21:43:47 -0700

    updated project tiles + pokemon search app
    Project tile now moves down after hovering.
    
    pokemon search app now shows the color of pokemon types rather than a gray background.

--- pokemonSearchAppJavaScript.js
+++ pokemonSearchAppJavaScript.js
@@ -12,8 +12,30 @@ const pokemonWeight = document.getElementById("weight");
 const pokemonHeight = document.getElementById("height");
 const pokemonSprite = document.getElementById("sprite");
 const pokemonTypes = document.getElementById("types");
+
 //iterate through stats
 const pokemonStatStrings = ["hp", "attack", "defense", "special-attack", "special-defense", "speed"];
+//match types to color hex
+const typesColorHex = {
+  "normal": "#A8A77A",
+  "fire": "#EE8130",
+  "water": "#6390F0",
+  "electric": "#F7D02C",
+  "grass": "#7AC74C",
+  "ice": "#96D9D6",
+  "fighting": "#C22E28",
+  "poison": "#A33EA1",
+  "ground": "#E2BF65",
+  "flying": "#A98FF3",
+  "psychic": "#F95587",
+  "bug": "#A6B91A",
+  "rock": "#B6A136",
+  "ghost": "#735797",
+  "dragon": "#6F35FC",
+  "dark": "#705746",
+  "steel": "#B7B7CE",
+  "fairy": "#D685AD"
+}
 
 const loadPokemonAPI = async () =>
 {
@@ -49,7 +71,7 @@ const displayStats = ({height, id, name, sprites, stats, types, weight}) => {
   pokemonSprite.src = sprites["front_default"];
   for (const i in types)
   {
-    pokemonTypes.innerHTML += `<li>${types[i].type.name}</li>`
+    pokemonTypes.innerHTML += `<li style="background: ${typesColorHex[types[i].type.name]}">${types[i].type.name}</li>`
   }
   for (const i in stats)
   {
