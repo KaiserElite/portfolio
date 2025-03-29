14a3df1686201253c90b8735401648ec12b225af
KaiserElite <erik.le.279@gmail.com>
2025-03-29 01:24:24 -0700

    added dropdown menu
    Added a dropdown menu for users to be able to navigate through the page using phones

--- portfolioJavaScript.js
+++ portfolioJavaScript.js
@@ -1,3 +1,22 @@
 let vh = window.innerHeight * .01;
 
 document.getElementById("introduction").style.setProperty("--vh", `${vh}px`);
+
+//dropdown menu code
+const dropdownButton = document.getElementById("dropdownButton");
+const overlayMenu = document.getElementById("overlay");
+const closeMenuButton = document.getElementById("closeMenuButton");
+const menuLink = document.getElementById("menuLink");
+
+function openMenu()
+{
+    overlayMenu.style.width = "100%";
+};
+
+function closeMenu()
+{
+    overlayMenu.style.width = "0%";
+}
+
+dropdownButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
